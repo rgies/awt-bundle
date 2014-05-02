3ee4e92c789f725cd0a8833d05b3125cc3c27c7f
Robert Gies <robert.gies@cocomore.com>
2014-05-02 09:20:37 +0200

    Improve: Config loader and remove duplicated code

--- DependencyInjection/AwtExtension.php
+++ DependencyInjection/AwtExtension.php
@@ -6,7 +6,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\Config\Definition\Processor;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -25,9 +24,6 @@ class AwtExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
-
-        $processor = new Processor();
-        $config = $processor->processConfiguration($configuration, $configs);
 
         $container->setParameter('awt.jira_connector', $config['jira_connector']);
     }
